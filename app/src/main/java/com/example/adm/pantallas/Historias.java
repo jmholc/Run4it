171445e81067f58fd01d0326a57ad82d0c8628ac
jmholc <juanholcman@gmail.com>
2017-09-27 10:41:15 -0300

    Descarga y descompresión. Cambios XML Login

--- app/src/main/java/com/example/adm/pantallas/Historias.java
+++ app/src/main/java/com/example/adm/pantallas/Historias.java
@@ -26,10 +26,14 @@ import android.widget.Toast;
 
 import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -204,9 +208,12 @@ public class Historias extends AppCompatActivity {
                                 stream.flush();
                                 stream.close();
                                 response.body().close();
-                                Decompress decompress = new Decompress(Environment.getExternalStorageDirectory() + "/Run4It/" + selected_file.substring(0, selected_file.length() - 4)+"/"+selected_file ,
+                                /*Decompress decompress = new Decompress(Environment.getExternalStorageDirectory() + "/Run4It/" + selected_file.substring(0, selected_file.length() - 4)+"/"+selected_file ,
                                         Environment.getExternalStorageDirectory() + "/Run4It/" + selected_file.substring(0, selected_file.length() - 4));
-                                decompress.unzip();
+                                decompress.unzip();*/
+                                unpackZip(Environment.getExternalStorageDirectory() + "/Run4It/" + selected_file.substring(0, selected_file.length() - 4)+"/",selected_file);
+                                File file = new File(Environment.getExternalStorageDirectory() + "/Run4It/" + selected_file.substring(0, selected_file.length() - 4)+"/"+selected_file);
+                                boolean deleted = file.delete();
                                 Log.d("Historias", "HISTORIA DESCOMPRIMIDA EXITOSAMENTE");
 
                             } catch (IOException e) {
@@ -227,6 +234,53 @@ public class Historias extends AppCompatActivity {
                 t.start();
             }
         });
+    }
+    private boolean unpackZip(String path, String zipname){
+        InputStream is;
+        ZipInputStream zis;
+        try
+        {
+            String filename;
+            is = new FileInputStream(path + zipname);
+            zis = new ZipInputStream(new BufferedInputStream(is));
+            ZipEntry ze;
+            byte[] buffer = new byte[1024];
+            int count;
+
+            while ((ze = zis.getNextEntry()) != null)
+            {
+                // zapis do souboru
+                filename = ze.getName();
+
+                // Need to create directories if not exists, or
+                // it will generate an Exception...
+                if (ze.isDirectory()) {
+                    File fmd = new File(path + filename);
+                    fmd.mkdirs();
+                    continue;
+                }
+
+                FileOutputStream fout = new FileOutputStream(path + filename);
+
+                // cteni zipu a zapis
+                while ((count = zis.read(buffer)) != -1)
+                {
+                    fout.write(buffer, 0, count);
+                }
+
+                fout.close();
+                zis.closeEntry();
+            }
+
+            zis.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
     }
 }
 class MyAdapter extends ArrayAdapter{
