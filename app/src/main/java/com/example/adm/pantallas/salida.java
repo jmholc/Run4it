cc19cb49f02b69eb72414f09311d5a26e1c4dde8
nicogute <32002163+nicogute@users.noreply.github.com>
2017-10-04 10:45:27 -0300

    Reproduccion continua de audio y posibilidad de seleccion de archivos

--- app/src/main/java/com/example/adm/pantallas/salida.java
+++ app/src/main/java/com/example/adm/pantallas/salida.java
@@ -1,5 +1,7 @@
 package com.example.adm.pantallas;
 
+import android.content.Context;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.media.MediaPlayer;
@@ -7,22 +9,41 @@ import android.view.*;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 public class salida extends AppCompatActivity {
     MediaPlayer reproductor;
-
+    int cont = 0;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_salida);
         reproductor = MediaPlayer.create(this,R.raw.music);
+
         //reproductor.setLooping(true);
+
 
 
         reproductor.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             public void onCompletion(MediaPlayer reproductor) {
 
                 Toast.makeText(getApplicationContext(),"Este audio ha terminado", Toast.LENGTH_LONG).show();
+                cont++;
+                reproductor.stop();
+                reproductor.reset();
+
+                try {
+                    String filename = "android.resource://" + getBaseContext().getPackageName() + "/raw/kalimba";
+                    reproductor.setDataSource(getBaseContext(), Uri.parse(filename));
+                    reproductor.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),"algo extraño", Toast.LENGTH_LONG).show();
+                }
+
+                reproductor.start();
+
             }
         });
 
