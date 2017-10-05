e806d94eccc0f6e8d27f96416ff4a983087c23d4
nicogute <nico.gutemberg@gmail.com>
2017-10-04 22:46:23 -0300

    Reproduccion de audios continua con orden numerico, BG music con volumen regulable y lectura de archivos .txt para el algoritmo de reestructurado

--- app/src/main/java/com/example/adm/pantallas/salida.java
+++ app/src/main/java/com/example/adm/pantallas/salida.java
@@ -9,37 +9,60 @@ import android.view.*;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import static com.example.adm.pantallas.R.raw.prueba;
 
 public class salida extends AppCompatActivity {
     MediaPlayer reproductor;
-    int cont = 0;
+    MediaPlayer bgmusic;
+    int maxVolume=100;
+    int volume = 0;
+    int cont = 1;
+    int runTime = 30;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_salida);
-        reproductor = MediaPlayer.create(this,R.raw.music);
+        Context ctx = getBaseContext();
+        InputStream is = ctx.getResources().openRawResource(prueba);
+        //String filename2 = "android.resource://" + getBaseContext().getPackageName() + "/raw/prueba.txt";
+        Scanner sc = new Scanner(is);
+        String linea = sc.next();
+        Toast.makeText(getApplicationContext(), linea, Toast.LENGTH_LONG).show();
+        sc.close();
+        reproductor = MediaPlayer.create(this, R.raw.audio1);
+        bgmusic = MediaPlayer.create(this, R.raw.bgmusic);
+        float log1 = (float) (Math.log(maxVolume - volume) / Math.log(maxVolume));
+        bgmusic.setVolume(1 - log1, 1 - log1);
+        bgmusic.start();
 
         //reproductor.setLooping(true);
-
 
 
         reproductor.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             public void onCompletion(MediaPlayer reproductor) {
 
-                Toast.makeText(getApplicationContext(),"Este audio ha terminado", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(),"Audio numero " + (cont+1), Toast.LENGTH_LONG).show();
                 cont++;
                 reproductor.stop();
                 reproductor.reset();
 
                 try {
-                    String filename = "android.resource://" + getBaseContext().getPackageName() + "/raw/kalimba";
+                    String filename = "android.resource://" + getBaseContext().getPackageName() + "/raw/audio" + cont;
                     reproductor.setDataSource(getBaseContext(), Uri.parse(filename));
                     reproductor.prepare();
                 } catch (IOException e) {
                     e.printStackTrace();
-                    Toast.makeText(getApplicationContext(),"algo extraño", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "algo extraño", Toast.LENGTH_LONG).show();
                 }
 
                 reproductor.start();
